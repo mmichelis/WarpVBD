65cdb0f79df379fe5d92f0b6a7ebffc42d7d52b2
Mike Michelis <michelism@ethz.ch>
2025-12-09 16:38:57 +0200

    Style: Color each simulation environment differently

--- examples/run_mass_spring_parallel.py
+++ examples/run_mass_spring_parallel.py
@@ -11,7 +11,7 @@ import sys
 sys.path.append("./src")
 
 from _utils import voxel2hex, hex2tets
-from _renderer import PbrtRenderer, export_mp4
+from _renderer import PbrtRenderer, export_mp4, filter_elements
 import vbd_solver
 
 # Matplotlib global settings
@@ -30,6 +30,7 @@ class MassSpringParallelSim:
     def __init__ (self, nsim=4, nx=7, density=1, youngs_modulus=6e3, poissons_ratio=0.45, dx_tol=1e-9, max_iter=1000, device="cuda"):
         ### Set up tetrahedral mesh
         dx = 1.0/nx
+        self.nsim = nsim
         # Split parallel simulations into xy grid
         nsimy = int(np.ceil(np.sqrt(nsim)))
         nsimx = int(np.ceil(nsim / nsimy))
@@ -51,13 +52,8 @@ class MassSpringParallelSim:
 
         vertices, hex_elements = voxel2hex(voxels, dx, dx, dx)
         self.wall_vertices, self.wall_elements = voxel2hex(wall_voxels, dx, dx, 0.1)
-        print(f"Voxel shape: {voxels.shape}.")
-
-        # Center x-axis
-        # center_x = 0.5 * (vertices[:,0].max() + vertices[:,0].min())
-        # vertices[:,0] -= center_x
-        # self.wall_vertices[:,0] -= center_x
         self.wall_vertices[:,2] += 2*nx*dx
+        print(f"Voxel shape: {voxels.shape}.")
 
         elements = hex2tets(hex_elements)
         n_vertices = vertices.shape[0]
@@ -66,18 +62,9 @@ class MassSpringParallelSim:
         self.elements = elements
         print(f"\033[95mGenerated tetrahedral mesh with {n_vertices} vertices and {n_elements} elements.\033[0m")
 
-        ### Set active mask and tip indices
-        active_mask = np.ones((vertices.shape[0],), dtype=bool)
-        tip_idx = []
-        for i in range(vertices.shape[0]):
-            if vertices[i,2] > 2*nx*dx-1e-3:
-                active_mask[i] = False
-            elif vertices[i,2] < 1e-3:
-                tip_idx.append(i)
-        self.tip_idx = tip_idx
-        print(f"Active vertices: {np.sum(active_mask)}/{vertices.shape[0]}")
-
-        ### Assign varying stiffnesses for each simulation
+        ### Assign varying stiffnesses for each simulation, and assign each vertex/element to a sim environment
+        self.vertex_sim_env = -1 * np.ones((vertices.shape[0],), dtype=int)
+        self.element_sim_env = -1 * np.ones((elements.shape[0],), dtype=int)
         youngs_moduli = np.zeros((n_elements,), dtype=np.float64)
         poissons_ratios = poissons_ratio * np.ones((n_elements,), dtype=np.float64)
         random_ym = np.random.uniform(-0.5, 0.5, size=(nsim,))
@@ -89,11 +76,26 @@ class MassSpringParallelSim:
             sim_idx = sim_x * nsimy + sim_y
             assert sim_idx < nsim, "Element assigned to non-existing simulation!"
 
+            self.element_sim_env[i] = sim_idx
+            for v_idx in elements[i]:
+                self.vertex_sim_env[v_idx] = sim_idx
+
             # Random Young's modulus between simulations
             youngs_moduli[i] = youngs_modulus * (1.0 + random_ym[sim_idx])
         print(f"\033[95mYoung's modulus range: {youngs_moduli.min()/1e3:.2f}kPa to {youngs_moduli.max()/1e3:.2f}kPa.\033[0m")
-            
 
+        ### Set active mask, tip indices, and assign each vertex to a sim environment
+        active_mask = np.ones((vertices.shape[0],), dtype=bool)
+        tip_idx = {i: [] for i in range(nsim)}
+        for i in range(vertices.shape[0]):
+            if vertices[i,2] > 2*nx*dx-1e-3:
+                active_mask[i] = False
+            elif vertices[i,2] < 1e-3:
+                tip_idx[self.vertex_sim_env[i]].append(i)
+        self.tip_idx = tip_idx
+        print(f"Active vertices: {np.sum(active_mask)}/{vertices.shape[0]}")
+
+        
         ### Initialize solver
         solution = wp.array(vertices, dtype=wp.vec3d, device=device)
         elements = wp.array(elements, dtype=wp.vec4i, device=device)
@@ -117,16 +119,14 @@ class MassSpringParallelSim:
         )
 
         # Adjust camera based on number of simulations
-        # self.camera_pos = (-0.01*(nsim//2), -0.75-0.2*(nsimx//2), 0.4)
-        # self.camera_lookat = (0.1+0.01*(nsim//2), 0.25, 0.2)
-        self.camera_pos = (-0.02*(nsimx//2), -1.0-0.02*(nsimy//2), 0.4)
-        self.camera_lookat = (0.1*(nsimx*(nx+padding_voxels)*dx/2), 0.1*(0.5+nsimy*(nx+padding_voxels)*dx/2), 0.1)
+        self.camera_pos = (-0.25-0.02*(nsimx//2), -1.0-0.02*(nsimy//2), 0.4)
+        self.camera_lookat = (0.1*(0.25+nsimx*(nx+padding_voxels)*dx/2), 0.1*(0.5+nsimy*(nx+padding_voxels)*dx/2), 0.1)
 
 
     def step (self, dt):
         return self.solver.step(self.solver.old_positions, wp.float64(dt))
 
-    def render (self, vertices, color_groups=False, filename=None, spp=4):
+    def render (self, vertices, filename=None, spp=4):
         """
         Rendering function using PBRT.
 
@@ -151,19 +151,19 @@ class MassSpringParallelSim:
         ]
         renderer = PbrtRenderer(options)
 
-        renderer.add_tri_mesh(vertices=vertices, elements=self.elements, render_edges=True, color="496d8a", transforms=transforms)
+        # Color each simulation differently
+        color_map = plt.get_cmap("tab10")
+        for sim_idx in range(self.nsim):
+            v, e = filter_elements(vertices, self.elements, np.argwhere(self.element_sim_env == sim_idx).flatten())
+            color = color_map(sim_idx % 10)
+            hex_color = "{:02x}{:02x}{:02x}".format(int(color[0]*255), int(color[1]*255), int(color[2]*255))
+
+            renderer.add_tri_mesh(vertices=v, elements=e, render_edges=True, color=hex_color, transforms=transforms)
+
         renderer.add_tri_mesh(objFile='asset/mesh/curved_ground.obj', texture_img='chkbd_24_0.7', transforms=[('s', 4)])
 
-        if color_groups:
-            # Color each vertex as a sphere according to its color group
-            n_colors = len(self.solver.color_groups)
-            cmap = plt.get_cmap('tab20', n_colors)
-            for c, cg in enumerate(self.solver.color_groups.numpy().values()):
-                for idx in cg:
-                    renderer.add_shape_mesh({'name': 'sphere', 'center': vertices[idx], 'radius': 0.005}, color=cmap(c)[:3], transforms=transforms)
-
         ### Add a wall plane as a hex mesh box.
-        renderer.add_hex_mesh(vertices=self.wall_vertices, elements=self.wall_elements, color="2ca02c", transforms=transforms)
+        renderer.add_hex_mesh(vertices=self.wall_vertices, elements=self.wall_elements, color="484848", transforms=transforms)
 
         renderer.render()
 
@@ -190,19 +190,20 @@ def main (args):
     n_substeps = 5
     dt = 1/fps/n_substeps
 
-    tip_positions = []
+    tip_positions = {i: [] for i in range(args.nsim)}
     for t in range(n_timesteps):
         start_time = time.time()
 
         for _ in range(n_substeps):
             solution = sim.step(dt)
-            tip_positions.append(solution.numpy()[sim.tip_idx].mean(axis=0))
+            for i in range(args.nsim):
+                tip_positions[i].append(solution.numpy()[sim.tip_idx[i]].mean(axis=0))
 
         end_time = time.time()
         print(f"---Timestep [{t:04d}/{n_timesteps}] ({1e3*dt*n_substeps:.1f}ms) in {1e3*(end_time - start_time):.3f}ms: Mean Positions: {solution.numpy().mean(axis=0)}")
 
         if args.render:
-            sim.render(solution.numpy(), filename=f"{output_folder}/frames/mass_spring_{t:03d}.png", spp=4)
+            sim.render(solution.numpy(), filename=f"{output_folder}/frames/mass_spring_parallel_{t:03d}.png", spp=4)
 
         # Plot Tip Displacements
         fig, ax = plt.subplots(figsize=(3,2))
@@ -213,15 +214,15 @@ def main (args):
         ax.grid()
         ax.set_xlim(0, n_seconds)
         ax.ticklabel_format(axis='both', style='sci', scilimits=(0,0))
-        fig.savefig(f"{output_folder}/displacement_mass_spring.png", dpi=300, bbox_inches='tight')
+        fig.savefig(f"{output_folder}/displacement_mass_spring_parallel.png", dpi=300, bbox_inches='tight')
         plt.close(fig)
 
     # Store as csv
-    np.savetxt(f"{output_folder}/displacement_mass_spring.csv", np.array(tip_positions), delimiter=",")
+    np.savetxt(f"{output_folder}/displacement_mass_spring_parallel.csv", np.array(tip_positions), delimiter=",")
 
     if args.render:
         # Render mp4
-        export_mp4(f"{output_folder}/frames", f"{output_folder}/mass_spring.mp4", fps=fps)
+        export_mp4(f"{output_folder}/frames", f"{output_folder}/mass_spring_parallel.mp4", fps=fps)
 
 
 if __name__ == "__main__":
